a7aaf901b92ffd172c2b9b38ecce960ed11e7949
Takuji Nishibayashi <takuji31@gmail.com>
2016-08-08 17:59:53 +0900

    peco select history

--- home/.zsh/02_function.zsh
+++ home/.zsh/02_function.zsh
@@ -8,3 +8,11 @@ function peco-src () {
 }
 zle -N peco-src
 bindkey '^S' peco-src
+
+peco-select-history() {
+    BUFFER=$(history 1 | sort -k1,1nr | perl -ne 'BEGIN { my @lines = (); } s/^\s*\d+\*?\s*//; $in=$_; if (!(grep {$in eq $_} @lines)) { push(@lines, $in); print $in; }' | peco --query "$LBUFFER")
+    CURSOR=${#BUFFER}
+    zle reset-prompt
+}
+zle -N peco-select-history
+bindkey '^r' peco-select-history
