da9de61eff15b4a9edf45c1e9c2e091727b124a7
Takuji Nishibayashi <takuji31@gmail.com>
2017-01-16 19:15:33 +0900

    config.fish

---
+++ home/.config/fish/config.fish
@@ -0,0 +1,5 @@
+set -x LC_ALL en_US.UTF-8
+set -x PATH /Applications/Sublime\ Text.app/Contents/SharedSupport/bin /Applications/MacVim.app/Contents/MacOS $HOME/.composer/vendor/bin/ $HOME/.anyenv/bin $HOME/local/bin $HOME/bin /usr/local/go/bin /usr/local/bin /usr/sbin /usr/bin /sbin /bin $PATH
+set -x EDITOR atom
+set -x PAGER less
+set -x MYSQL_PS1 '\U DB:\d DATE: \D MySQL: \v  \n>\_'
