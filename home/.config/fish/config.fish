448b581e7cbd2fb0aebeb4e1aef3752b485e4687
Takuji Nishibayashi <takuji31@gmail.com>
2018-08-17 14:22:10 +0900

    Remove anyenv and Add plenv

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,15 +1,16 @@
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
 add_path /usr/local/go/bin /usr/local/bin
-add_path $HOME/.composer/vendor/bin/ $HOME/.anyenv/bin $HOME/local/bin $HOME/bin
+add_path $HOME/.composer/vendor/bin/ $HOME/local/bin $HOME/bin
+add_path $HOME/.plenv/bin
 add_path /Applications/MacVim.app/Contents/MacOS
 set -x EDITOR atom
 set -x PAGER less
 set -x MYSQL_PS1 '\U DB:\d DATE: \D MySQL: \v  \n>\_'
 
-# anyenv
-if test -d $HOME/.anyenv
-  source (anyenv init -|psub)
+# plenv
+if test -d $HOME/.plenv
+  source (plenv init -|psub)
 end
 
 if test -x (which go)
