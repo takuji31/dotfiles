a8566cae10fdbda969f80a4f851e59ad1af5e590
Takuji Nishibayashi <takuji31@gmail.com>
2018-08-17 14:22:55 +0900

    atom -w

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -4,7 +4,7 @@ add_path /usr/local/go/bin /usr/local/bin
 add_path $HOME/.composer/vendor/bin/ $HOME/local/bin $HOME/bin
 add_path $HOME/.plenv/bin
 add_path /Applications/MacVim.app/Contents/MacOS
-set -x EDITOR atom
+set -x EDITOR 'atom -w'
 set -x PAGER less
 set -x MYSQL_PS1 '\U DB:\d DATE: \D MySQL: \v  \n>\_'
 
