ecd55140370649371c7215550480280b068dd0b8
Takuji Nishibayashi <takuji31@gmail.com>
2018-12-08 22:17:48 +0900

    add_path udpated

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,12 +1,19 @@
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
-add_path /usr/local/go/bin /usr/local/bin
-add_path $HOME/.composer/vendor/bin/ $HOME/local/bin $HOME/bin
+add_path /usr/local/go/bin
+add_path /usr/local/bin
+add_path $HOME/.composer/vendor/bin/
+add_path $HOME/.anyenv/bin
 add_path $HOME/.plenv/bin
+add_path $HOME/local/bin
+add_path $HOME/bin
 add_path /Applications/MacVim.app/Contents/MacOS
-add_path /usr/local/share/android-sdk/bin /usr/local/share/android-sdk/tools /usr/local/share/android-sdk/tools/bin /usr/local/share/android-sdk/build-tools
+add_path /usr/local/share/android-sdk/bin
+add_path /usr/local/share/android-sdk/tools
+add_path /usr/local/share/android-sdk/tools/bin
+add_path /usr/local/share/android-sdk/build-tools
 add_path /usr/local/share/android-sdk/platform-tools/
-set -x EDITOR 'atom -w'
+set -x EDITOR 'vim'
 set -x PAGER less
 set -x MYSQL_PS1 '\U DB:\d DATE: \D MySQL: \v  \n>\_'
 
@@ -23,7 +30,8 @@ end
 if test -x (which go)
   set -x GOROOT (go env GOROOT)
   set -x GOPATH $HOME/.go
-  add_path $GOPATH/bin $GOROOT/bin
+  add_path $GOPATH/bin
+  add_path $GOROOT/bin
 end
 
 if test -f $HOME/.fishconfig_local.fish
