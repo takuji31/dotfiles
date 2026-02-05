d2d43acf606881f5d4dd8f1b1a11a7fa6eac021d
Takuji Nishibayashi <takuji31@gmail.com>
2026-02-06 03:39:32 +0900

    feat(fish): add zp command for project-specific zellij layouts
    - zp: start zellij with .zellij.kdl in current directory
    - zp new: create .zellij.kdl from template (fzf selection)
    - zp edit: edit .zellij.kdl with $EDITOR

--- dot_config/fish/conf.d/zellij.fish
+++ dot_config/fish/conf.d/zellij.fish
@@ -4,4 +4,44 @@ if command -q zellij
     alias za="zellij attach"
     alias zl="zellij list-sessions"
     alias zk="zellij kill-session"
+
+    # Zellij project layout (.zellij.kdl) manager
+    function zp
+        set -l layout_dir ~/.config/zellij/layouts
+
+        switch $argv[1]
+            case edit
+                set -l editor $EDITOR
+                test -z "$editor"; and set editor vim
+                $editor .zellij.kdl
+            case new
+                if test -f .zellij.kdl
+                    echo "Error: .zellij.kdl already exists" >&2
+                    return 1
+                end
+                set -l templates (find $layout_dir -name '*.kdl' -type f 2>/dev/null | sort)
+                if test -z "$templates"
+                    echo "Error: No templates found in $layout_dir" >&2
+                    return 1
+                end
+                set -l selected (printf '%s\n' $templates | fzf --prompt="Select template: ")
+                if test -n "$selected"
+                    cp "$selected" .zellij.kdl
+                    echo "Created .zellij.kdl from "(basename $selected)
+                else
+                    echo "Cancelled" >&2
+                    return 1
+                end
+            case ''
+                if test -f .zellij.kdl
+                    zellij --layout ./.zellij.kdl
+                else
+                    echo "Error: .zellij.kdl not found in current directory" >&2
+                    return 1
+                end
+            case '*'
+                echo "Usage: zp [new|edit]" >&2
+                return 1
+        end
+    end
 end
